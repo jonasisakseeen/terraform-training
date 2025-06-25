90ef5abb962c2b6e177bee032c1e4e0195e339b2
Jonas Isaksen <jonisa@jotron.com>
2025-06-25 12:39:36 +0200

    changes to names

--- lab2/main.tf
+++ lab2/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "storage_account_one" {
-    source              = "./modules/storage_account"
+    source              = "./modules/storrage_account"
     storage_account_name = "${var.storage_account_name}one"
     location            = var.location
     account_tier        = "Standard"
@@ -12,7 +12,7 @@ module "storage_account_one" {
 }
 
 module "storage_account_two" {
-    source              = "./modules/storage_account"
+    source              = "./modules/storrage_account"
     storage_account_name = "${var.storage_account_name}two"
     location            = var.location
     account_tier        = "Standard"
